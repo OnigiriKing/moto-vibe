0ef3f1c582ba09351697f5235083c8808b616a22
OnigiriKing <oooNikita87@gmail.com>
2023-07-28 16:43:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./dist/style.css";
+import { Routes } from "react-router-dom";
+import NavBar from "./pages/navBar/navBar";
+export default function App() {
+  return (
+    <div className="App">
+    <NavBar />
+    <Routes>
+      
+    </Routes>
+    </div>
+  );
+}
---
+++ src/pages/navBar/navBar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { navSvg } from "../../svg/navSvg";
+
+export default function NavBar() {
+  return (
+    <div id="nav-bar">
+      <div className="nav-wrapper">
+        <div className="nav-logo">
+          <h3>
+            M<p>o</p>t<p>o</p>
+          </h3>
+          <h3>Vibe</h3>
+          {navSvg("#3F84E5", 40).logo}
+        </div>
+        <div className="nav-menu">
+          <Link to={"/"} children="Home" />
+          <Link to={"/About"} children="About" />
+          <Link to={"/Models"} children="Bike Models" />
+          <Link to={"/Reviews"} children="Testimonials" />
+          <Link to={"/Contact"} children="Contact" />
+        </div>
+        <div className="login-register">
+          <Link children="Sign up" />
+          <Link children="Register" />
+        </div>
+      </div>
+    </div>
+  );
+}
