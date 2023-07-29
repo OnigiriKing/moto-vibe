bbfa27c4865285fed6ed705464ebd7cc066c595f
OnigiriKing <oooNikita87@gmail.com>
2023-07-29 10:06:17 +0700

    welcome screen update

--- src/pages/homePage/components/Welcome.jsx
+++ src/pages/homePage/components/Welcome.jsx
@@ -1,11 +1,21 @@
 import { homeSvg } from "../../../svg/homeSvg";
+import bikeImg from "../../../img/welcome-bike.png"
 
 export default function Welcome() {
   return (
     <div className="welcome-screen">
       {homeSvg()}
-      <div></div>
-      <div></div>
+      <div className="welcome-des">
+        <h3>Explore the city on the bike</h3>
+        <h1>Ride, Explore, and Embrace Bangkok </h1>
+        <p>
+          Ride the Streets of Bangkok with us - Your Ultimate Motorbike Rental
+          Hub
+        </p>
+      </div>
+      <div className="welcome-img">
+        <img src={bikeImg} />
+      </div>
     </div>
   );
 }
