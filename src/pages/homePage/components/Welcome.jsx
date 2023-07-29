e910a8d51151e37699f61d3856ce69b24f66eca7
OnigiriKing <oooNikita87@gmail.com>
2023-07-29 10:31:56 +0700

    welcome screen ready (almost)

--- src/pages/homePage/components/Welcome.jsx
+++ src/pages/homePage/components/Welcome.jsx
@@ -4,16 +4,25 @@ import bikeImg from "../../../img/welcome-bike.png"
 export default function Welcome() {
   return (
     <div className="welcome-screen">
-      {homeSvg()}
       <div className="welcome-des">
         <h3>Explore the city on the bike</h3>
-        <h1>Ride, Explore, and Embrace Bangkok </h1>
+        <h1>
+          Ride, <strong>Explore</strong>, and Embrace Bangkok
+        </h1>
         <p>
           Ride the Streets of Bangkok with us - Your Ultimate Motorbike Rental
           Hub
         </p>
+        <div className="welcome-links">
+          <a href="#">
+            Book Bike {homeSvg(20).check}
+          </a>
+          <a>
+            Learn More {homeSvg(20).arrow}
+          </a>
+        </div>
       </div>
-      <div className="welcome-img">
+      <div className="welcome-img ">
         <img src={bikeImg} />
       </div>
     </div>
--- src/svg/homeSvg.jsx
+++ src/svg/homeSvg.jsx
@@ -1,20 +1,36 @@
+export function homeSvg(width) {
+  const svg = {
+    check: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={width}
+        viewBox="0 0 32 32"
+      >
+        <path
+          fill="currentColor"
+          d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z"
+        />
+        <path
+          fill="none"
+          d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"
+        />
+      </svg>
+    ),
+    arrow: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={width}
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M11.273 3.687a1 1 0 1 1 1.454-1.374l8.5 9a1 1 0 0 1 0 1.374l-8.5 9.001a1 1 0 1 1-1.454-1.373L19.125 12l-7.852-8.313Z"
+        />
+      </svg>
+    ),
+  };
 
-
-export function homeSvg() {
-
-    return (
-      <div class="custom-shape">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-            class="shape-fill"
-          ></path>
-        </svg>
-      </div>
-    );
+  return svg;
 }
