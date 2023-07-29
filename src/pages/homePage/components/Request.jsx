0d47e32f5cc7a65e781cc3284415ce484aebf87f
OnigiriKing <oooNikita87@gmail.com>
2023-07-29 20:29:28 +0700

    request form visual update

--- src/pages/homePage/components/Request.jsx
+++ src/pages/homePage/components/Request.jsx
@@ -8,32 +8,30 @@ export function Request() {
       <div id="home-request">
         <div className="request-wrapper">
           <h2>Book a bike</h2>
-          <div className="request-form">
-            <form>
-              <div>
-                <label>Select a bike</label>
-                <select></select>
-              </div>
-              <div>
-                <label>Pick-up</label>
-                <select></select>
-              </div>
-              <div>
-                <label>Drop-off</label>
-                <select></select>
-              </div>
-              <div>
-                <label>Pick-up date</label>
-                <input type="date"></input>
-              </div>
-              <div>
-                <label>Drop-off date</label>
-                <input type="date"></input>
-              </div>
+          <form className="request-form">
+            <div className="request-select">
+              <label>Select a bike</label>
+              <select></select>
+            </div>
+            <div className="request-select">
+              <label>Pick-up</label>
+              <select></select>
+            </div>
+            <div className="request-select">
+              <label>Drop-off</label>
+              <select></select>
+            </div>
+            <div className="request-date">
+              <label>Pick-up date</label>
+              <input type="date"></input>
+            </div>
+            <div className="request-date">
+              <label>Drop-off date</label>
+              <input type="date"></input>
+            </div>
 
-              <button children="Search" />
-            </form>
-          </div>
+            <button children="Search" />
+          </form>
         </div>
       </div>
     );
