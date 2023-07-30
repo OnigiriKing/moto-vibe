e1c333a2919b00f17a21bb28c2086d72f9b4dee4
OnigiriKing <oooNikita87@gmail.com>
2023-07-30 13:16:48 +0700

    Advantage page ready

--- src/pages/homePage/components/Advantages.jsx
+++ src/pages/homePage/components/Advantages.jsx
@@ -1,4 +1,4 @@
-
+import { advantagesSvg } from "../../../svg/advantagesSvg";
 
 export default function Advantages() {
 
@@ -8,9 +8,30 @@ export default function Advantages() {
         <h2>Explore Bangkok</h2>
         <h1>Best bike rental service</h1>
         <div className="advantages-list">
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="advantage">
+            <div>{advantagesSvg(55).bike}</div>
+            <h3>Select a bike</h3>
+            <p>
+              Choose from a diverse fleet of premium motorcycles, tailored to
+              fit your style and preferences.
+            </p>
+          </div>
+          <div className="advantage">
+            <div>{advantagesSvg(55).support}</div>
+            <h3>24/7 support</h3>
+            <p>
+              Feel confident and cared for with our reliable 24/7 customer
+              support.
+            </p>
+          </div>
+          <div className="advantage">
+            <div>{advantagesSvg(55).moving}</div>
+            <h3>Enjoy bangkok</h3>
+            <p>
+              Zip through the bustling streets and hidden gems of this vibrant
+              city on our top-notch bikes.
+            </p>
+          </div>
         </div>
       </div>
     );
