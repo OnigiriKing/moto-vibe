3132899e418eab314c53abd483184aa563d17767
OnigiriKing <oooNikita87@gmail.com>
2023-07-31 18:24:51 +0700

    Reviews Screen cascade is ready

---
+++ src/pages/homePage/components/Reviews.jsx
@@ -0,0 +1,28 @@
+export default function Reviews() {
+  return (
+    <div id="reviews-screen">
+      <div className="review-screen-wrapper">
+        <div className="reviews-screen-des">
+          <h2>Unfiltered Testimonials</h2>
+          <h1>Your Words, Our Pride</h1>
+          <p>
+            We take pride in showcasing the voices of our clients, providing a
+            platform for their honest feedback and testimonials. Join us in
+            celebrating the joy of exceptional customer experiences that have
+            made a lasting impact.
+          </p>
+        </div>
+        <div>
+          <div>
+            <h2></h2>
+            <div></div>
+          </div>
+          <div>
+            <h2></h2>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
