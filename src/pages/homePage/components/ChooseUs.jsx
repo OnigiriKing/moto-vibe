d50bf2e3c49f748b53f0d219b9aa792bb25901c8
OnigiriKing <oooNikita87@gmail.com>
2023-07-31 14:47:50 +0700

    ChooseUs.jsx created

---
+++ src/pages/homePage/components/ChooseUs.jsx
@@ -0,0 +1,59 @@
+
+
+export function ChooseUs() {
+
+    return (
+      <div id="choose-us-screen">
+        <div className="choose-us-wrapper">
+          <div className="choose-us-text">
+            <h2>Why choose us</h2>
+            <h1>Best bike rental in Bangkok</h1>
+            <p>
+              Experience the freedom to travel at your own pace and uncover the
+              hidden gems of Bangkok that are often missed by conventional
+              tourists. Rent a motorbike with us and rev up your Bangkok
+              experience with unforgettable memories and an adrenaline-fueled
+              adventure like no other!
+            </p>
+            <a>Rent Bike</a>
+          </div>
+
+          <div className="choose-us-all-reasons">
+            <div className="choose-us-reason">
+              <div></div>
+              <div>
+                <h2>Safety First</h2>
+                <p>
+                  Safety is our top priority, and we provide helmets and safety
+                  gear to ensure a secure and enjoyable ride.
+                </p>
+              </div>
+            </div>
+
+            <div className="choose-us-reason">
+              <div></div>
+              <div>
+                <h2>No Hidden Charges</h2>
+                <p>
+                  Explore Bangkok freely and without worries with our
+                  all-inclusive pricing policy.
+                </p>
+              </div>
+            </div>
+
+            <div className="choose-us-reason">
+              <div></div>
+              <div>
+                <h2>Best Quality Bikes</h2>
+                <p>
+                  Expertly maintained, our bikes offer
+                  the utmost in comfort, safety, and performance, ensuring you
+                  have a smooth and enjoyable ride.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
