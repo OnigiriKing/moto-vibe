afb483fb3f25eaa3ee37d58b8dc0f55fc47e8369
OnigiriKing <oooNikita87@gmail.com>
2023-08-01 19:54:30 +0700

    Advertisement screen is created

---
+++ src/pages/homePage/components/Advertisment.jsx
@@ -0,0 +1,26 @@
+export default function Advertisment() {
+
+   return (
+     <div id="advertisment-screen">
+       <div className="Advertisment-wrapper">
+         <h1>Supercharge your experience with our app</h1>
+         <h4>
+           Discover the joy of two-wheeled freedom with our motorbike rental
+           app. Download now and let the ride of a lifetime begin!
+         </h4>
+         <div>
+           <a
+             href="https://play.google.com"
+             target="_blank"
+             rel="noreferrer"
+           ></a>
+           <a
+             href="https://www.apple.com/app-store/"
+             target="_blank"
+             rel="noreferrer"
+           ></a>
+         </div>
+       </div>
+     </div>
+   );
+}
