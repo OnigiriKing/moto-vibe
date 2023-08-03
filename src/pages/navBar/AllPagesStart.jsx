4cf1826a3ef36ed388c2f0dfb9b0f80cedeeaeec
OnigiriKing <oooNikita87@gmail.com>
2023-08-03 18:53:23 +0700

    AllPagesStart update

--- src/pages/navBar/AllPagesStart.jsx
+++ src/pages/navBar/AllPagesStart.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function AllPagesStart({page}) {
 
    return (
-     <div className="all-pages-start-screen">
+     <div id="all-pages-start-screen">
        <div className="all-pages-start-wrapper">
          <div className="all-pages-start-text">
-           <h3>{page}</h3>
-           <h4>
+           <h1>{page}</h1>
+           <h3>
              <Link to={"/"} children="Home" /> / {page}
-           </h4>
+           </h3>
          </div>
        </div>
      </div>
