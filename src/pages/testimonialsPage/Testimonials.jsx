11b3a5331136d6eb3ef9d1c63732834832f06c9e
OnigiriKing <oooNikita87@gmail.com>
2023-08-03 19:05:53 +0700

    Testimonials page ready

--- src/pages/testimonialsPage/Testimonials.jsx
+++ src/pages/testimonialsPage/Testimonials.jsx
@@ -1,10 +1,12 @@
 import AllPagesStart from "../navBar/AllPagesStart"
+import Reviews from "../homePage/components/Reviews"
 
 export default function Testimonials() {
 
     return (
       <div id="testimonials-page">
         <AllPagesStart page="Testimonials" />
+        <Reviews />
       </div>
     );
 }
