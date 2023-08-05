31a8a0bd186d38b91a614e39ed1355c7031f9b38
OnigiriKing <oooNikita87@gmail.com>
2023-08-05 13:50:12 +0700

    Bike models page start

---
+++ src/pages/bikeModelsPage/BikeModels.jsx
@@ -0,0 +1,14 @@
+import AllPagesStart from "../navBar/AllPagesStart";
+import AllPagesFooter from "../footerPage/AllPagesFooter";
+
+export default function BikeModels() {
+  return (
+    <div>
+      <AllPagesStart />
+      <div id="bike-models-page">
+        
+      </div>
+      <AllPagesFooter />
+    </div>
+  );
+}
---
+++ src/pages/bikeModelsPage/components/BikeDes.jsx
@@ -0,0 +1,8 @@
+export default function BikeDes() {
+
+   return (
+       <div className="bike-des-wrapper">
+        
+       </div>
+    );
+}
