42123fd6f20ef2b30facc6e263605b1b8ba73d7f
OnigiriKing <oooNikita87@gmail.com>
2023-08-07 22:41:38 +0700

    nav svg added, @media test

--- src/svg/navSvg.jsx
+++ src/svg/navSvg.jsx
@@ -25,6 +25,38 @@ export function navSvg(color, width) {
         </g>
       </svg>
     ),
+    menuBtn: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={width}
+        viewBox="0 0 48 48"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-width="4"
+        >
+          <path stroke-linejoin="round" d="M40 28L24 40L8 28" />
+          <path d="M8 10H40" />
+          <path d="M8 18H40" />
+        </g>
+      </svg>
+    ),
+    closeBtn: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={width}
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"
+        />
+      </svg>
+    ),
   };
 
   return navSvg;
