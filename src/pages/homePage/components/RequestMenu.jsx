f4eb2b8f23e677f000ade52fdcc6a38a6ccd0a4c
OnigiriKing <oooNikita87@gmail.com>
2023-09-02 15:17:51 +0700

    Request menu style complete

--- src/pages/homePage/components/RequestMenu.jsx
+++ src/pages/homePage/components/RequestMenu.jsx
@@ -32,103 +32,111 @@ export default function RequestMenu({info}) {
         <div className="request-menu-bg" />
         <div className="request-menu-screen">
           <div className="request-menu-des">
-            <div className="request-menu-top">
-              <h2>COMPLETE RESERVATION</h2>
-              <button>{navSvg(25).closeBtn}</button>
-            </div>
-            <div className="request-menu-mid">
-              <h3>
-                Upon completing this reservation enquiry, you will receive:
-              </h3>
-              <p>
-                Your rental voucher to produce on arrival at the rental desk and
-                a toll-free customer support number.
-              </p>
-            </div>
-            <div className="request-menu-main">
-              <div className="menu-main-time">
-                <div className="main-date-location">
-                  <span>
-                    <h3>Location & Date</h3>
-                  </span>
-                  <div className="location-info">
-                    {requestSvg(20).calendar}
-                    <div>
-                      <h3>Pick-Up Date & Time</h3>
-                      <p>
-                        {info.pickDate} / <input type="time" />
-                      </p>
-                    </div>
-                  </div>
-                  <div className="location-info">
-                    {requestSvg(20).calendar}
-                    <div>
-                      <h3>Drop-Off Date & Time</h3>
-                      <p>
-                        {info.dropDate} / <input type="time" />
-                      </p>
-                    </div>
-                  </div>
-                  <div className="location-info">
-                    {requestSvg(20).location}
-                    <div>
-                      <h3>Pick-Up Location</h3>
-                      <p>{info.pickUp}</p>
-                    </div>
-                  </div>
-                  <div className="location-info">
-                    {requestSvg(20).location}
-                    <div>
-                      <h3>Drop-Off Location</h3>
-                      <p>{info.dropOff}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="menu-main-bike">
-                  <h3>
-                    Bike:
-                    <b>{info.bikeType}</b>
-                  </h3>
-                  <img src={bikeImg} />
-                </div>
-              </div>
-              <div className="menu-main-info">
-                <h3>Personal Information</h3>
-                <div className="main-info-personal">
-                  <InputField
-                    name="First Name"
-                    holder="Enter your first name"
-                  />
-                  <InputField name="Last Name" holder="Enter your last name" />
-                  <InputField
-                    name="Phone Number"
-                    holder="Enter your phone number"
-                  />
-                  <InputField name="Age" holder="18" type="number" />
-                </div>
-                <div className="main-info-mail">
-                  <InputField name="Email" holder="Enter your Email" />
-                  <InputField
-                    name="Address"
-                    holder="Enter your street address"
-                  />
-                </div>
-                <div className="main-info-address">
-                  <InputField
-                    name="City"
-                    holder="Enter your City"
-                  />
-                  <InputField
-                    name="Zip Code"
-                    holder="Enter your Zip Code"
-                  />
-                </div>
-                <input type="checkbox"/>
-              </div>
-              <div className="menu-main-complete">
-                <button>Reserve now</button>
+            <div className="request-menu-wrapp">
+              <div className="request-menu-top">
+                <h2>COMPLETE RESERVATION</h2>
+                <button>{navSvg(25).closeBtn}</button>
               </div>
             </div>
+            <div className="request-menu-wrapp">
+              <div className="request-menu-mid">
+                <h3>
+                  Upon completing this reservation enquiry, you will receive:
+                </h3>
+                <p>
+                  Your rental voucher to produce on arrival at the rental desk
+                  and a toll-free customer support number.
+                </p>
+              </div>
+            </div>
+            <div className="request-menu-wrapp">
+              <div className="request-menu-main">
+                <div className="menu-main-time">
+                  <div className="main-date-location">
+                    <span>
+                      <h3>Location & Date</h3>
+                    </span>
+                    <div className="location-info">
+                      {requestSvg(20).calendar}
+                      <div>
+                        <h3>Pick-Up Date & Time</h3>
+                        <p>
+                          {info.pickDate} / <input type="time" />
+                        </p>
+                      </div>
+                    </div>
+                    <div className="location-info">
+                      {requestSvg(20).calendar}
+                      <div>
+                        <h3>Drop-Off Date & Time</h3>
+                        <p>
+                          {info.dropDate} / <input type="time" />
+                        </p>
+                      </div>
+                    </div>
+                    <div className="location-info">
+                      {requestSvg(20).location}
+                      <div>
+                        <h3>Pick-Up Location</h3>
+                        <p>{info.pickUp}</p>
+                      </div>
+                    </div>
+                    <div className="location-info">
+                      {requestSvg(20).location}
+                      <div>
+                        <h3>Drop-Off Location</h3>
+                        <p>{info.dropOff}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="menu-main-bike">
+                    <h3>
+                      Bike:
+                      <b>{info.bikeType}</b>
+                    </h3>
+                    <img src={bikeImg} />
+                  </div>
+                </div>
+                <div className="menu-main-info">
+                  <h3>Personal Information</h3>
+                  <div className="main-info-personal">
+                    <InputField
+                      name="First Name"
+                      holder="Enter your first name"
+                    />
+                    <InputField
+                      name="Last Name"
+                      holder="Enter your last name"
+                    />
+                    <InputField
+                      name="Phone Number"
+                      holder="Enter your phone number"
+                    />
+                    <InputField name="Age" holder="18" type="number" />
+                  </div>
+                  <div className="main-info-mail">
+                    <InputField name="Email" holder="Enter your Email" />
+                    <InputField
+                      name="Address"
+                      holder="Enter your street address"
+                    />
+                  </div>
+                  <div className="main-info-address">
+                    <InputField name="City" holder="Enter your City" />
+                    <InputField name="Zip Code" holder="Enter your Zip Code" />
+                  </div>
+                  <div className="main-info-check">
+                    <input type="checkbox" />
+                    <p>Please send me latest news and updates</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+              <div className="request-menu-wrapp">
+                <div className="menu-main-complete">
+                  <button>Reserve now</button>
+                </div>
+              </div>
           </div>
         </div>
       </>
