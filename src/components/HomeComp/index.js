b3d9b7e397611d73c022098a8156268b524bf06d
OnigiriKing <oooNikita87@gmail.com>
2023-09-20 17:29:17 +0700

    HomeComp created

---
+++ src/components/HomeComp/index.js
@@ -0,0 +1,3 @@
+import HomeComp from "./HomePage";
+
+export default HomeComp;
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,26 +1,10 @@
-import Welcome from "./components/Welcome"
-import { Request } from "./components/Request"
-import Advantages from "./components/Advantages"
-import { Models } from "./components/BikeModels"
-import { ChooseUs } from "./components/ChooseUs"
-import Reviews from "./components/Reviews"
-import Questions from "./components/Questions"
-import Application from "./components/Application";
-
-
+import HomeComp from "components/HomeComp";
 
 export default function HomePage() {
 
     return (
-      <div id="home-page">
-        <Welcome />
-        <Request />
-        <Advantages />
-        <Models />
-        <ChooseUs />
-        <Reviews />
-        <Questions />
-        <Application />
-      </div>
+      <>
+      <HomeComp />
+      </>
     );
 }
